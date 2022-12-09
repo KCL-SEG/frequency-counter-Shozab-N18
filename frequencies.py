139983a0cd99e15df51e76d9f3bccd12b4de44ac
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2022-12-09 12:30:45 +0000

    First attempt

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    list = items
+    for i in list:
+        count = 0
+        for x in list:
+            if (i == x):
+                list.remove(x)
+                count += 1
+        frequencies.update(i,count)
+        list.remove(i)
     return frequencies
